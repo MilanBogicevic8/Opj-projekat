29cbce9c1b2678631946147b2818116751cd181d
Milica Jevtović <milica.jev@gmail.com>
2025-09-25 00:14:55 +0200

    Dodati su izvučeni pravni podaci u originalnom stanju i konvertovani na latinicu, zajedno sa korišćenom skriptom. Dodata je sripta za tokenizovanje podataka i kreiran fajl. Fajl je takođe konvertovan u Exel fajl.

---
+++ pravni_podaci/cirilica_u_latinicu.py
@@ -0,0 +1,23 @@
+import os
+from transliterate import translit
+
+# folder gde se nalaze txt fajlovi
+INPUT_FOLDER = "izvuceno"
+OUTPUT_FOLDER = "izvuceno_latinica"
+
+os.makedirs(OUTPUT_FOLDER, exist_ok=True)
+
+for filename in os.listdir(INPUT_FOLDER):
+    if filename.endswith(".txt"):
+        input_path = os.path.join(INPUT_FOLDER, filename)
+        output_path = os.path.join(OUTPUT_FOLDER, filename)
+
+        with open(input_path, "r", encoding="utf-8") as f:
+            content = f.read()
+
+        converted = translit(content, "sr", reversed=True)
+
+        with open(output_path, "w", encoding="utf-8") as f:
+            f.write(converted)
+
+        print(f"{filename} konvertovan.")

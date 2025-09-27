3e59d96bd315b22bd380c5d08667040f20bafbd1
Aleksa Vuckovic <aleksa.vuckovic36@gmail.com>
2025-09-27 23:05:57 +0200

    add literature annotations by aleksa

--- pythonProject/scraping/twitter/convert.py
+++ pythonProject/scraping/twitter/convert.py
@@ -2,8 +2,14 @@ import pandas
 from scraping.twitter.const import *
 import csv
 
-df = pandas.read_excel(ANNOTATIONS_EXCEL, header=None)
-for i, row in df.iterrows():
-    if not pandas.isna(row[0]) and not row[0].startswith("#") and pandas.isna(row[10]): 
-        row[10] = "O"
-df.to_csv(ANNOTATIONS_CONLLU, sep='\t', index=False, header=False, quoting=csv.QUOTE_NONE)
+def excel_to_conllu(input: Path, output: Path):
+    df = pandas.read_excel(input, header=None)
+    for i, row in df.iterrows():
+        if not pandas.isna(row[0]) and not row[0].startswith("#") and pandas.isna(row[10]): 
+            row[10] = "O"
+    df.to_csv(output, sep='\t', index=False, header=False, quoting=csv.QUOTE_NONE)
+
+def conllu_to_excel(input: Path, output: Path):
+    table = [it.split("\t") if not it.startswith("#") and not len(it) == 0 else [it] for it in input.read_text().split("\n")]
+    df = pandas.DataFrame(table)
+    df.to_excel(output, index=False, header=False)

285f7ff5e0f95b46019236ac69dc6a632819d99c
Aleksa Vuckovic <aleksa.vuckovic36@gmail.com>
2025-09-28 16:33:26 +0200

    Add more literature tokens

--- pythonProject/scraping/twitter/convert.py
+++ pythonProject/scraping/twitter/convert.py
@@ -5,7 +5,7 @@ import csv
 def excel_to_conllu(input: Path, output: Path):
     df = pandas.read_excel(input, header=None)
     for i, row in df.iterrows():
-        if not pandas.isna(row[0]) and not row[0].startswith("#") and pandas.isna(row[10]): 
+        if not pandas.isna(row[0]) and not str(row[0]).startswith("#") and pandas.isna(row[10]): 
             row[10] = "O"
     df.to_csv(output, sep='\t', index=False, header=False, quoting=csv.QUOTE_NONE)
 
